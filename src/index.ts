7e1dd383dcb58b178ac9cbf33f876c1aa23ca813
omchaudhary007 <omchaudhary0730@gmail.com>
2023-10-11 20:14:00 +0530

    minor fixes

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,10 @@ const app = express();
 const PORT = env.PORT || 999;
 const MONGO_URI = env.MONGO_URI;
 
-app.use(cors());
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true
+}));  
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
